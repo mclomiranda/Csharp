10075a3f9cfdc6ea7d77792cc814e25d3b06678e
Mclomiranda <mclomiranda@gmail.com>
2021-06-29 22:53:26 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Digite uma opção:");
+           Console.WriteLine("1- para inserir um novo usuário");
+           Console.WriteLine("2- para lista usuário");
+           Console.WriteLine("3- para calcular média geral");
+           Console.WriteLine("X  para sair");
+           Console.WriteLine();
+
+            string opcao = Console.ReadLine ();
+
+            while(opcao.ToUpper() != "X")
+            {
+                switch (opcao)
+                {
+                case "1":
+                    Console.WriteLine("aluno");
+                    break;
+                
+                case "2":   
+                      Console.WriteLine("lista");
+                    break;
+
+                case "3":
+                    Console.WriteLine("média");
+                    break;
+
+                default:
+                throw new ArgumentOutOfRangeException ();
+                }
+
+                Console.WriteLine("Digite uma opção:");
+                Console.WriteLine("1- para inserir um novo usuário");
+                Console.WriteLine("2- para lista usuário");
+                Console.WriteLine("3- para calcular média geral");
+                Console.WriteLine("X  para sair");
+                Console.WriteLine();
+
+                opcao = Console.ReadLine ();
+
+            }
+                   
+        
+        }
+
+
+    }
+}
